8b1d1f0b31355e335fa70e40af51deccd2e6c62c
iCEt33 <peedee826@gmail.com>
2026-01-14 21:47:03 +0100

    Move API to Vercel serverless functions folder

--- api/pxp-signup.js
+++ api/pxp-signup.js
@@ -1,6 +1,16 @@
-import { google } from 'googleapis';
+const { google } = require('googleapis');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -41,4 +51,4 @@ export default async function handler(req, res) {
     console.error('API Error:', error);
     return res.status(500).json({ success: false, error: error.message });
   }
-}
+};
